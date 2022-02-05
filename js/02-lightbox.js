572107a343ad8bf46570f49196a65d087d8cb489
leonid12z <lelik_ceh@ukr.net>
2022-02-05 16:38:41 +0200

    d/z-07 na proverky

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = elementGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', onGalleryClick);
+
+function elementGalleryMarkup(items) {
+   return galleryItems.map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+   }).join('');
+};
+
+function onGalleryClick(sample) {
+
+   sample.preventDefault();
+
+   const isGalleryImageLi = sample.target.classLiist.contains('gallery__image')
+   if (!isGalleryImageLi) {
+      return;
+   };
+   
+   let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+};
